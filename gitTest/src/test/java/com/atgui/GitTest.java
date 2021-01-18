fda0d766b5d615f1be48908c9282dc17ed9e321c
lijian <1690070109@qq.com>
2021-01-18 19:36:18 +0800

    美年gitTest

---
+++ gitTest/src/test/java/com/atgui/GitTest.java
@@ -0,0 +1,11 @@
+package com.atgui;
+
+/**
+ * @author lijian
+ * @create 2021-01-18 19:34
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git TEST");
+    }
+}
